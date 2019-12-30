5983fe1052dbe6e3f6a5560046e8bcafaa373747
Dobromir Stefanov <dobromirstefanov96@gmail.com>
2019-12-30 19:16:04 +0200

    Move the rest of the logic to sub and add main function

--- architecture/run-script.pl
+++ architecture/run-script.pl
@@ -2,7 +2,6 @@
 use strict;
 use warnings;
 
-# Used modules
 use JSON::XS qw(encode_json decode_json);
 use Try::Tiny;
 use IPC::Open3 qw(open3);
@@ -12,51 +11,51 @@ use constant INPUT_TESTS_FILE => 'input-tests.json';
 use constant TEST_FILE => 'runtest';
 use constant RESULT_FILE => 'result-data.json';
 
-my $json_tests = parse_json_file(INPUT_TESTS_FILE);
+sub run_tests {
+	my $json_tests = parse_json_file(INPUT_TESTS_FILE);
 
-my ($pid, $chld_outline, $chld_proc, $chld_answer); # Used to pass and read the data from test file
+	my ($pid, $chld_outline, $chld_proc, $chld_answer); # Used to pass and read the data from test file
 
-my @result_data = (); # Save the result from test script
+	my $counter = 1; # Counter for input tests
 
-my $counter = 1; # Counter for input tests
+	foreach my $input_tests (@{$json_tests}) {
+		# Open file for input tests
+		try {
+			$pid = open3(\*CHLD_WRITE, \*CHLD_READ, 0, './' . TEST_FILE);
+		} catch {
+			print "Unable to open " . TEST_FILE . " for I/O operations\n";
+			exit 4;
+		};
 
-foreach my $input_tests (@{$json_tests}) {
-	# Open file for input tests
-	try {
-		$pid = open3(\*CHLD_WRITE, \*CHLD_READ, 0, './' . TEST_FILE);
-	} catch {
-		print "Unable to open " . TEST_FILE . " for I/O operations\n";
-		exit 4;
-	};
+		# Send the input string to CHILD STDIN
+		foreach my $input_test (@{$input_tests}) {
+			print CHLD_WRITE "$input_test\n";
+		}
 
-	# Send the input string to CHILD STDIN
-	foreach my $input_test (@{$input_tests}) {
-		print CHLD_WRITE "$input_test\n";
+		$chld_answer = '';
+		# Read the output from the test script and add it to the array
+		while (1) {
+			$chld_proc = waitpid($pid, WNOHANG);
+
+			if ($chld_proc == -1) { # Problem with child process
+				print "Child process error: " . ($? >> 8);
+				exit 5;
+			}
+
+			if ($chld_proc) { # Child process is ready
+				last;
+			}
+
+			$chld_outline = <CHLD_READ>; # Get output line from child process
+
+			if ($chld_outline) {
+				$chld_answer .= $chld_outline; # Append and construct the output
+			}
+		}
+
+		save_current_test_result($chld_answer, $counter);
+		$counter++;
 	}
-
-	$chld_answer = '';
-	# Read the output from the test script and add it to the array
-	while (1) {
-		$chld_proc = waitpid($pid, WNOHANG);
-
-		if ($chld_proc == -1) { # Problem with child process
-			print "Child process error: " . ($? >> 8);
-			exit 5;
-		}
-
-		if ($chld_proc) { # Child process is ready
-			last;
-		}
-
-		$chld_outline = <CHLD_READ>; # Get output line from child process
-
-		if ($chld_outline) {
-			$chld_answer .= $chld_outline; # Append and construct the output
-		}
-	}
-
-	save_current_test_result($chld_answer, $counter);
-	$counter++;
 }
 
 sub parse_json_file {
@@ -108,5 +107,11 @@ sub save_current_test_result {
 
 	close $FH;
 }
+
+sub __main() {
+	run_tests();
+}
+
+__main();
 
 exit 0;
