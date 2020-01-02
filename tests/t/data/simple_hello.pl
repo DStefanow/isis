8ea9383c31c703ff80baf74e526e385d7e5d74e8
Dobromir Stefanov <dobromirstefanov96@gmail.com>
2020-01-03 00:08:19 +0200

    Add some data for testing runner script

---
+++ tests/t/data/simple_hello.pl
@@ -0,0 +1,15 @@
+#!/usr/bin/perl
+use strict;
+use warnings;
+
+print "Enter first name: ";
+my $first_name = <STDIN>;
+chomp $first_name;
+
+print "Enter second name: ";
+my $second_name = <STDIN>;
+chomp $second_name;
+
+print "Hello!\n";
+print "Your first name is $first_name!\n";
+print "And your second name is: $second_name!\n";
